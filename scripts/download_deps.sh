d973701b4c3135d3e60eae6b0ec62397fdf2ed52
Lkishor123 <laawanyak.29@gmail.com>
2025-06-13 00:00:48 +0530

    GGML: 1.3

--- scripts/download_deps.sh
+++ scripts/download_deps.sh
@@ -15,7 +15,8 @@ echo "Dependencies downloaded to $(pwd)/model and $(pwd)/tokenizer"
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 EXTERNAL_DIR="${ROOT_DIR}/external"
 GGML_REPO="https://github.com/ggerganov/ggml.git"
-GGML_COMMIT="d58ce750aac398568457db9c69882492c515d488"
+# MODIFIED: Updated ggml commit to a version compatible with the bert.cpp commit.
+GGML_COMMIT="a3c634c436f5631a3d24e548cc6a1202e85e3d7a"
 BERTCPP_REPO="https://github.com/ggerganov/bert.cpp.git"
 BERTCPP_COMMIT="7f084fa6bdab9939b383495666617ceabe1522db"
 
@@ -34,7 +35,7 @@ clone_repo() {
 
 # Clone bert.cpp first
 clone_repo "$BERTCPP_REPO" "${EXTERNAL_DIR}/bert.cpp" "$BERTCPP_COMMIT"
-# MODIFIED: Clone ggml as a subdirectory of bert.cpp
+# Clone ggml as a subdirectory of bert.cpp
 clone_repo "$GGML_REPO" "${EXTERNAL_DIR}/bert.cpp/ggml" "$GGML_COMMIT"
 
 
