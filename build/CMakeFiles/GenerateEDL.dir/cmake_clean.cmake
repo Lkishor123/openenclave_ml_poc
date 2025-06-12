f6bc16365c592281502949b54795d5873910adb0
Ubuntu <lk@accvm.rbpxpz4vlcsuhp2cslmfgudzic.bx.internal.cloudapp.net>
2025-06-12 15:40:58 +0000

    Added Build to commit edl generated file

---
+++ build/CMakeFiles/GenerateEDL.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenerateEDL"
+  "edl_generated/.dir_stamp"
+  "edl_generated/enclave_args.h"
+  "edl_generated/enclave_t.c"
+  "edl_generated/enclave_t.h"
+  "edl_generated/enclave_u.c"
+  "edl_generated/enclave_u.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/GenerateEDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
